793d41bdae493be375fe9c1170996bc62931d48c
Mehul Patel <mehul.21aug@gmail.com>
2016-11-28 12:34:03 -0500

    Kevin Bacon Number program added.

---
+++ ProgrammingPractice/src/baconNumber/Edge.java
@@ -0,0 +1,8 @@
+package baconNumber;
+
+/**
+    Edge position for graph.
+    @param <T> Type of element on edge.
+*/
+public interface Edge<T> extends Position<T> {
+}
---
+++ ProgrammingPractice/src/baconNumber/Vertex.java
@@ -0,0 +1,8 @@
+package baconNumber;
+
+/**
+    Vertex position for graph.
+    @param <T> Type of element on vertex.
+*/
+public interface Vertex<T> extends Position<T> {
+}
