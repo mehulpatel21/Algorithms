8239ac59df7d2514a3f46e6af1afcdd234fa7dd0
Mehul Patel <mehul.21aug@gmail.com>
2016-11-28 02:13:41 -0500

    quicksort added

--- ProgrammingPractice/src/sorting/QuickSort.java
+++ ProgrammingPractice/src/sorting/QuickSort.java
@@ -1,50 +1,63 @@
 package sorting;
 
 public class QuickSort {
+	private int[] numbers;
+	private int len;
 	
-	void quickSort(int[] arr, int low, int high){
-		int partIndex= partition(arr, low, high);
-		quickSort(arr, low, partIndex-1);
-		quickSort(arr, partIndex+1, high);
+	private void sort(int[] arr){
+		if(arr.length ==0 || arr == null){
+			return;
+		}
+		this.numbers = arr;
+		len = arr.length;
+		quickSort(0, len-1);
 	}
 	
-	int partition(int[] arr, int low, int high){
-		int pivot = arr[high];
-		int i = low-1;
-		for(int j=low; j<=high-1; j++){
-			if(arr[j] < arr[pivot]){
+	private void quickSort(int low, int high){
+		int pivot = numbers[low + (high-low)/2];
+		int i = low, j = high;
+		
+		while(i <= j){
+			while(numbers[i] < pivot){
 				i++;
-				int temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+			}
+			while(numbers[j] > pivot){
+				j--;
+			}
+			if(i<=j){
+				int temp = numbers[i];
+				numbers[i] = numbers[j];
+				numbers[j] = temp;
+				i++;
+				j--;
+			}
+			
+			if(low < j){
+				quickSort(low, j);
+			}
+			if(i < high){
+				quickSort(i, high);	
 			}
 		}
-		int temp = arr[i+1];
-		arr[i+1] = arr[high];
-		arr[high] = temp;
-		
-		return i+1;
-		
 	}
 	
-	 static void printArray(int arr[])
-	    {
-	        int n = arr.length;
-	        for (int i=0; i<n; ++i)
-	            System.out.print(arr[i]+" ");
-	        System.out.println();
-	    }
-
-	public static void main(String[] args) {
-		int arr[] = {10, 7, 8, 9, 1, 5};
-        int n = arr.length;
- 
-        QuickSort ob = new QuickSort();
-        ob.quickSort(arr, 0, n-1);
- 
-        System.out.println("sorted array");
-        printArray(arr);
-
+	public static void printArray(int[] arr){
+		for(int i=0; i < arr.length; i++){
+			System.out.println(arr[i] + " ");
+		}
+		System.out.println();
 	}
-
+	
+	public static void main(String[] args){
+		int[] arr = {10,1, 100000, 0, 2, 100, 11};
+		
+		System.out.println("Before sorting");
+		printArray(arr);
+		
+		QuickSort obj = new QuickSort();
+		obj.sort(arr);
+		
+		System.out.println("After sorting");
+		printArray(arr);
+	}
 }
