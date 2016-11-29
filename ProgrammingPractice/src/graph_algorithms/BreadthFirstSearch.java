bc854973144b4266d23363464d86430de1732d8d
Mehul Patel <mehul.21aug@gmail.com>
2016-11-29 01:03:04 -0500

    changes to BFS.

--- ProgrammingPractice/src/graph_algorithms/BreadthFirstSearch.java
+++ ProgrammingPractice/src/graph_algorithms/BreadthFirstSearch.java
@@ -6,41 +6,60 @@ import java.util.LinkedList;
 
 public class BreadthFirstSearch {
 	
-		private int V;
-		private LinkedList<Integer> adj[];
+	private int V;   // No. of vertices
+    private LinkedList<Integer> adj[]; //Adjacency Lists
+ 
+    @SuppressWarnings({ "unchecked", "rawtypes" })
+	// Constructor
+    BreadthFirstSearch(int v)
+    {
+        V = v;
+        adj = new LinkedList[v];
+        for (int i=0; i<v; ++i)
+            adj[i] = new LinkedList();
+    }
+ 
+    // Function to add an edge into the graph
+    void addEdge(int v,int w)
+    {
+        adj[v].add(w);
+    }
 		
-		@SuppressWarnings({ "unchecked", "rawtypes" })
-		BreadthFirstSearch(int v){
-			V = v;
-			adj = new LinkedList[v];
-			for(int i=0; i<v; i++){
-				adj[i] = new LinkedList();
-			}
-		}
-		
-		void addEdge(int v, int w){
-			adj[v].add(w);
-		}
-		
-		void breadthFirstSearch(int s){
-			boolean visited[] = new boolean[V];
-			LinkedList<Integer> queue = new LinkedList<Integer>();
-			visited[s] = true;
-			queue.add(s);
-			
-			while(queue.size() != 0){
-				s=queue.poll();
-				System.out.println(s + " ");
-				Iterator<Integer> itr = adj[s].listIterator();
-				while(itr.hasNext()){
-					int n = itr.next();
-					if(!visited[n]){
-						visited[n] = true;
-						queue.add(n);
-					}
-				}
-			}
-		}
+		// prints BFS traversal from a given source s
+	    void breadthFirstSearch(int s)
+	    {
+	        // Mark all the vertices as not visited(By default
+	        // set as false)
+	        boolean visited[] = new boolean[V];
+	 
+	        // Create a queue for BFS
+	        LinkedList<Integer> queue = new LinkedList<Integer>();
+	 
+	        // Mark the current node as visited and enqueue it
+	        visited[s]=true;
+	        queue.add(s);
+	 
+	        while (queue.size() != 0)
+	        {
+	            // Dequeue a vertex from queue and print it
+	            s = queue.poll();
+	            System.out.print(s+" ");
+	 
+	            // Get all adjacent vertices of the dequeued vertex s
+	            // If a adjacent has not been visited, then mark it
+	            // visited and enqueue it
+	            Iterator<Integer> i = adj[s].listIterator();
+	            while (i.hasNext())
+	            {
+	                int n = i.next();
+	                if (!visited[n])
+	                {
+	                    visited[n] = true;
+	                    queue.add(n);
+	                }
+	            }
+	        }
+	    }
 	
 	public static void main(String[] args) {
 		BreadthFirstSearch g = new BreadthFirstSearch(4);
